2e3572e20036a2b52d58ab26c503564ee096d81e
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-05-28 12:20:41 -0300

    Função do Caso de Uso: Adicionar Produto

---
+++ produto.py
@@ -0,0 +1,28 @@
+estoque = []
+
+def adicionar_produto(nome, tipo, quantidade):
+    if produto_ja_cadastrado(nome):
+        return True
+
+    if estoque_lotado(quantidade):
+        return True
+    
+    novo_produto = {
+        "nome": nome,
+        "tipo": tipo, 
+        "quantidade": quantidade
+    }
+
+    estoque.append(novo_produto)
+    return True
+
+def produto_ja_cadastrado(nome):
+    for produto in estoque:
+        if produto["nome"] == nome:
+            return True
+    return False
+
+def estoque_lotado(quantidade):
+    limite_estoque = 100
+    quantidade_total = sum(produto["quantidade"] for produto in estoque)
+    return quantidade_total + quantidade > limite_estoque
