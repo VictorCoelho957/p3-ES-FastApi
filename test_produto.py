2cff0d885765adbec6d2eeda4d4f08bf1ea3e9c7
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-05-31 18:54:24 -0300

    Refatoração do Teste do Caso de Uso: Adicionar Produto

--- test_produto.py
+++ test_produto.py
@@ -2,18 +2,23 @@ import unittest
 from produto import adicionar_produto
 
 class TestProduto(unittest.TestCase):
+
+    produto_1 = "Produto 1"
+    produto_2 = "Produto 2"
+    tipo_1 = "Tipo 1"
+    tipo_2 = "Tipo 2"
+
     def test_adicionar_produto_sucesso(self):
         # Teste: Adicionar um novo produto ao estoque com sucesso
-        resultado = adicionar_produto("Produto 1", "Tipo 1", 10)
+        resultado = adicionar_produto( self.produto_1, self.tipo_1, 10)
         self.assertTrue(resultado)
 
     def test_adicionar_produto_duplicado(self):
         # Teste: Tentar adicionar um produto já existente no estoque
-        resultado = adicionar_produto("Produto 2", "Tipo 1", 5)
+        resultado = adicionar_produto(self.produto_2, self.tipo_1, 5)
         self.assertTrue(resultado)
-        print(resultado)
 
     def test_adicionar_produto_estoque_lotado(self):
         # Teste: Tentar adicionar um produto quando o estoque estiver cheio
-        resultado = adicionar_produto("Produto 2", "Tipo 2", 100)
+        resultado = adicionar_produto(self.produto_2, self.tipo_2, 100)
         self.assertTrue(resultado)
