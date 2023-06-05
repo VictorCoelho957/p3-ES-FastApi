f46bff393058cdb68013b319796ae3aa701dd8f6
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-06-05 12:14:25 -0300

    Refatoração de Código: Variáveis

--- test_produto.py
+++ test_produto.py
@@ -5,6 +5,7 @@ class TestProduto(unittest.TestCase):
 
     produto_1 = "Produto 1"
     produto_2 = "Produto 2"
+    produto_3 = "Produto 3"
     tipo_1 = "Tipo 1"
     tipo_2 = "Tipo 2"
 
@@ -39,14 +40,14 @@ class TestProduto(unittest.TestCase):
 
         resultado_atual = listar_produtos()
         self.assertEqual(len(resultado_atual), 1)
-        self.assertNotIn({"nome": "Produto 1", "tipo": "Tipo 1", "quantidade": 10}, resultado_atual)
+        self.assertNotIn({"nome": self.produto_1, "tipo": self.tipo_1, "quantidade": 10}, resultado_atual)
 
     def test_atualizar_produto(self):
         # Teste: Atualizar informações de um produto no estoque
         resultado_anterior = listar_produtos()
         self.assertEqual(len(resultado_anterior), 2)
 
-        resultado = atualizar_produto("Produto 1", "Novo Produto", "Novo Tipo", 20)
+        resultado = atualizar_produto(self.produto_1, "Novo Produto", "Novo Tipo", 20)
         self.assertTrue(resultado)
 
         resultado_atual = listar_produtos()
@@ -56,13 +57,13 @@ class TestProduto(unittest.TestCase):
 
     def test_listar_produto_especifico(self):
         # Teste: Exibir informações de um produto específico
-        resultado = listar_produto_especifico("Produto 2")
+        resultado = listar_produto_especifico(self.produto_2)
         self.assertIsNotNone(resultado)
-        self.assertEqual(resultado["nome"], "Produto 2")
-        self.assertEqual(resultado["tipo"], "Tipo 1")
+        self.assertEqual(resultado["nome"], self.produto_2)
+        self.assertEqual(resultado["tipo"], self.tipo_1)
         self.assertEqual(resultado["quantidade"], 5)
 
     def test_listar_produto_especifico_produto_nao_encontrado(self):
         # Teste: Tentar exibir informações de um produto que não existe no estoque
-        resultado = listar_produto_especifico("Produto 3")
+        resultado = listar_produto_especifico(self.produto_3)
         self.assertIsNone(resultado)
